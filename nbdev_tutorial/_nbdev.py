93467c5d7cac5d4e504aae0bcdb0aeeffbef52b6
Don Winter <don@wintr.de>
2022-06-15 11:33:55 +0200

    fixed bug

--- nbdev_tutorial/_nbdev.py
+++ nbdev_tutorial/_nbdev.py
@@ -2,8 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"HelloSayer": "00_core.ipynb",
-         "say_hello": "00_core.ipynb"}
+index = {"say_hello": "00_core.ipynb",
+         "HelloSayer": "00_core.ipynb"}
 
 modules = ["core.py"]
 
